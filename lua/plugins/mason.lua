397cffd5c127996ae22ca5efe875a09dc9680f9d
wol4aravio <panovskiy.v@yandex.ru>
2025-10-30 10:08:25 +0300

    basic lsp

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					-- Lua
+					"lua_ls",
+				},
+			})
+		end,
+	},
+}

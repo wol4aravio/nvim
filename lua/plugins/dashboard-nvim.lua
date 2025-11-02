5cb75b5e00f26cba496f272dd1d2fbd4da720c23
wol4aravio <panovskiy.v@yandex.ru>
2025-11-02 13:54:52 +0300

    01_dashboards

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,19 @@
+return {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+        require('dashboard').setup({
+            theme = "hyper",
+            config = {
+                week_header = { enable = true }, -- show day name
+                project = { enable = true, limit = 3, action = '' }, -- show projects block
+                mru = { enable = false }, -- hide recently opened files
+                shortcut = {
+                    { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+                    { desc = "󰩈 Quit", group = "@property", action = "q", key = "q" },
+                },
+            },
+        })
+    end,
+    dependencies = {{ "nvim-tree/nvim-web-devicons" }},
+}

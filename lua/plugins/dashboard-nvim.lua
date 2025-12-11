379f1d9b8097f503dd7ebefcd10bb7f1b5b37d95
wol4aravio <panovskiy.v@yandex.ru>
2025-12-11 09:04:08 +0300

    increased recent project counter

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,19 +1,19 @@
 return {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    config = function()
-        require('dashboard').setup({
-            theme = "hyper",
-            config = {
-                week_header = { enable = true }, -- show day name
-                project = { enable = true, limit = 3, action = 'Telescope find_files cwd=' }, -- show projects block
-                mru = { enable = false }, -- hide recently opened files
-                shortcut = {
-                    { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-                    { desc = "󰩈 Quit", group = "@property", action = "q", key = "q" },
-                },
-            },
-        })
-    end,
-    dependencies = {{ "nvim-tree/nvim-web-devicons" }},
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				week_header = { enable = true }, -- show day name
+				project = { enable = true, limit = 7, action = "Telescope find_files cwd=" }, -- show projects block
+				mru = { enable = false }, -- hide recently opened files
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{ desc = "󰩈 Quit", group = "@property", action = "q", key = "q" },
+				},
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
